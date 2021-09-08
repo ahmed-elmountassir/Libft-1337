db1476a8667a715dcaa968bb66a119a5c94a76c8
mountassir_007 <ahmedelmountassir10@gmail.com>
2021-09-08 15:21:36 +0100

    strcmp

--- 1s-part/ft_strcmp.c
+++ 1s-part/ft_strcmp.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int
+int	ft_strcmp(char *st1, char *st2)
+{
+	int	i;
+
+	i = 0;
+	while (st1[i] && st2[i] && st1[i] == st2[i])
+		i++;
+	return ((unsigned char)st1[i] - (unsigned char)st2);
+}
