172c1e24631fbe7c363c228433e206a9f6f32dd9
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-09-10 12:53:57 +0100

    tolower [Done]

--- 1s-part/done/ft_tolower.c
+++ 1s-part/done/ft_tolower.c
@@ -6,11 +6,13 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 15:37:14 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/09/08 15:37:14 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/09/10 12:53:00 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_tolower(int cara)
+int	ft_tolower(int	cara)
 {
-	return (cara + 32);
+	if (cara >= 65 && cara <= 90)
+		return (cara + 32);
+	return (cara);
 }
