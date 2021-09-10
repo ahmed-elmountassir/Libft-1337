e60a571bf0020ee9cd0fcb9013902e649c58efa2
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-09-10 12:58:21 +0100

    main

--- 1s-part/done/main.c
+++ 1s-part/done/main.c
@@ -2,8 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "ft_strcat.c"
+#include "ft_atoi.c"
+#include "ft_strcpy.c"
+#include "ft_strcmp.c"
+#include "ft_toupper.c"
+#include "ft_tolower.c"
+#include "ft_strlen.c"
 int main()
 {
+	/*
+	char te1[4];
+	char te2[] = "ahme15";
+	printf("%s\n", strcpy(te1, te2));
+	char t1[4];
+	char t2[] = "ahme15";
+	printf("%s\n", ft_strcpy(t1, t2));
+	*/
+	printf("%d", ft_strlen("ahmed el mountassir"));
 	return (0);
 }
