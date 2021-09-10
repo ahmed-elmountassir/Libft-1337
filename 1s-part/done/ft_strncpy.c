019e01075548968f05e8792395344b99bf6ea858
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-09-10 12:44:48 +0100

    strncpy [Done]

--- 1s-part/done/ft_strncpy.c
+++ 1s-part/done/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 15:13:13 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/09/08 15:13:13 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/09/09 20:55:33 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@ char	*ft_strncpy(char *st1, char *st2, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (st1[i] && st2[i] && i < size)
+	while (st2[i] && i < size)
 	{
-		str1[i] = st2[i];
+		st1[i] = st2[i];
 		i++;
 	}
+	while (i < size)
+	{
+		st1[i] = '\0';
+		i++;
+	}
+	
 	return (st1);
 }
