0ed5d31f57ff518501c2b8c3ae06e02ba6b82a94
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-09-17 12:48:39 +0100

    main

--- 1s-part/done/main.c
+++ 1s-part/done/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "ft_strcat.c"
 #include "ft_atoi.c"
 #include "ft_strcpy.c"
@@ -9,16 +10,24 @@
 #include "ft_toupper.c"
 #include "ft_tolower.c"
 #include "ft_strlen.c"
+#include "ft_strncmp.c"
+#include "ft_strchr.c"
+#include "ft_strrchr.c"
+#include "ft_strdup.c"
+
 int main()
 {
 	/*
-	char te1[4];
-	char te2[] = "ahme15";
-	printf("%s\n", strcpy(te1, te2));
-	char t1[4];
-	char t2[] = "ahme15";
-	printf("%s\n", ft_strcpy(t1, t2));
-	*/
-	printf("%d", ft_strlen("ahmed el mountassir"));
+	char te1[] = "ahmed67";
+	char te2[] = "ahmed15hh";
+	printf("%d\n", strncmp(te1, te2, 8));
+	char t1[] = "ahmed67";
+	char t2[] = "ahmed15hh";
+	printf("%d\n", ft_strncmp(t1, t2, 8));
+
+//	printf("%d", ft_strlen("ahmed el mountassir"));
+*/
+	char h[] = "ahmed el mountassir 36";
+	printf("%s", ft_strrchr(h, 32));
 	return (0);
 }
