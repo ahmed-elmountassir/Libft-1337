33d7b66525f69c02818a281bda3dd8b39e118402
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-11-13 00:34:33 +0100

    clear a list

---
+++ ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahel-mou <ahel-mou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 18:19:38 by ahel-mou          #+#    #+#             */
+/*   Updated: 2021/11/12 20:28:00 by ahel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+	t_list	*next;
+
+	tmp = *lst;
+	while (tmp)
+	{
+		next = tmp->next;
+		ft_lstdelone(tmp, del);
+		tmp = next;
+	}
+	*lst = NULL;
+}
